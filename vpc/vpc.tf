ff51f8b4d5f2892f9dd898aa1a9f94a58ae59bf6
JagadeeshkumarS <129980043+JagadeeshkumarS@users.noreply.github.com>
2023-05-06 14:57:52 +0530

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,8 +1,8 @@
 #Declaring AWS Provider and credentials
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAQJTGRA6VEBDYYHED"
-  secret_key = "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"
+  access_key = "....."
+  secret_key = "....."
 }
 #######################################################
 #Creating Production VPC with CIDR: 10.0.0.0/16
