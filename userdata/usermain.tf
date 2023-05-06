b78fe47392653e0234d8c2b900651e891014d5be
JagadeeshkumarS <129980043+JagadeeshkumarS@users.noreply.github.com>
2023-05-06 14:56:43 +0530

    Update usermain.tf

--- userdata/usermain.tf
+++ userdata/usermain.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQJTGRA6VEBDYYHED"
-  secret_key = "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"
+  access_key = "......"
+  secret_key = "......"
 }
 resource "aws_instance" "userdata" {
    ami = "ami-0557a15b87f6559cf"
